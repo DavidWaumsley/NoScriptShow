c0c0041118ff91982113f64384991687cad08679
David Waumsley <davidwaumsley@gmail.com>
2025-01-29 13:22:41 +0530

    Initial commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "No Script Show",
+	url: "https://example.com/",
+	language: "en-gb",
+	description: "I am writing about my experiences as a naval navel-gazer.",
+	author: {
+		name: "David Waumsley",
+		email: "youremailaddress@example.com",
+		url: "https://example.com/about-me/"
+	}
+}
---
+++ content/blog/ep2.md
@@ -0,0 +1,11 @@
+---
+title: The big picture.
+description: How our future episodes will attempt to tell the story of the web.
+date: 2024-01-17
+tags: ["podcast"]
+permalink: "/2{{slugify }}/"
+videoid: AQAw17sNO6c
+---
+ 
+
+
